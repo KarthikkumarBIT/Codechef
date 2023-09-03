bd61af1c7e8b987ddd5c6e2d7f816fb79a9abe43
KarthikkumarBIT <130389076+KarthikkumarBIT@users.noreply.github.com>
2023-09-03 21:18:35 +0530

    Create DOREWARD.py

---
+++ WEEK3/DOREWARD.py
@@ -0,0 +1,10 @@
+N=int(input())
+for i in range(N):
+    A=int(input())
+    if(A<=3):
+        print("BRONZE")
+    elif 3<A<=6:
+        print("SILVER")
+    else:
+        print("GOLD")
+        
